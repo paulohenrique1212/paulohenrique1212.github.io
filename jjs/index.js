b9e542be7cf281451d57051a400e8114df6e6a69
paulohenrique1212 <119430223+paulohenrique1212@users.noreply.github.com>
2023-01-03 23:03:18 -0300

    Add files via upload

---
+++ jjs/index.js
@@ -0,0 +1,60 @@
+const no01 = document.querySelector(".no1");
+const no02 = document.querySelector(".no2");
+const no03 = document.querySelector(".no3");
+const no04 = document.querySelector(".no4");
+const no05 = document.querySelector(".no5");
+const accept = document.querySelector(".yes-accept")
+const yes = document.querySelector(".yes");
+
+no03.addEventListener("mouseover", trocaNo);
+
+function trocaNo(){
+    no03.classList.remove("show");
+    no01.classList.add("show");
+}
+
+no01.addEventListener("mouseover", trocaNo2);
+
+function trocaNo2(){
+    no01.classList.remove("show");
+    no04.classList.add("show");
+}
+
+no04.addEventListener("mouseover", trocaNo3);
+
+function trocaNo3(){
+    no04.classList.remove("show");
+    no02.classList.add("show");
+}
+
+no02.addEventListener("mouseover", trocaNo4);
+
+function trocaNo4(){
+    no02.classList.remove("show");
+    no05.classList.add("show");
+}
+
+no05.addEventListener("mouseover", trocaNo5);
+
+function trocaNo5(){
+    no05.classList.remove("show");
+    no03.classList.add("show");
+}
+
+
+
+
+yes.addEventListener("click", aceita);
+
+function aceita(){
+    no01.classList.remove("show");
+    no02.classList.remove("show");
+    no03.classList.remove("show");
+    no04.classList.remove("show");
+    no05.classList.remove("show");
+
+    accept.classList.add("show");
+}
+
+
+// LUCIFER PASSOU POR AQUI
