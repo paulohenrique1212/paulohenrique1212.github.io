a48a6be24d7b7c33f8b11296e68449652c01bfe7
paulohenrique1212 <119430223+paulohenrique1212@users.noreply.github.com>
2023-01-08 21:02:38 -0300

    Update index.js

--- jjs/index.js
+++ jjs/index.js
@@ -5,6 +5,8 @@ const no04 = document.querySelector(".no4");
 const no05 = document.querySelector(".no5");
 const accept = document.querySelector(".yes-accept")
 const yes = document.querySelector(".yes");
+const text = document.querySelector(".text");
+const anime = document.querySelector(".anime")
 
 no03.addEventListener("mouseover", trocaNo);
 
@@ -47,12 +49,14 @@ function trocaNo5(){
 yes.addEventListener("click", aceita);
 
 function aceita(){
-    no01.classList.remove("show");
-    no02.classList.remove("show");
-    no03.classList.remove("show");
-    no04.classList.remove("show");
-    no05.classList.remove("show");
+    no01.classList.add("hidden");
+    no02.classList.add("hidden");
+    no03.classList.add("hidden");
+    no04.classList.add("hidden");
+    no05.classList.add("hidden");
     yes.classList.add("hidden");
+    text.classList.add("hidden");
+    anime.classList.add("show")
 
     accept.classList.add("show");
 }
